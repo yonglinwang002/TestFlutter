33e3dbdd172801eb1b577611b52a5e2232910022
王永林 <wangyonglin@maoyan.com>
2018-09-25 16:22:54 +0800

    Initial Commit

---
+++ TestFlutter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFlutter
+//
+//  Created by 王永林 on 2018/9/25.
+//  Copyright © 2018 xxstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFlutter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFlutter
+//
+//  Created by 王永林 on 2018/9/25.
+//  Copyright © 2018 xxstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
